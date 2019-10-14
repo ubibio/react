235f2c07aea6a4035940cf42bbc963b303865cd5
ubibio <sgolilla1@naver.com>
2019-10-15 01:01:24 +0900

    banila

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+
+class App extends Component {
+  render() {
+    return (
+      <TodoListTemplate>
+        템플릿 완성
+      </TodoListTemplate>
+    );
+  }
+}
+
+export default App;
